cf45a6998485a9ec2cb4c4d0881454ba76d08ba1
originalop <rswami5579@gmail.com>
2025-03-19 17:04:28 +0530

    Removed not working file

--- Rename_File_By_Creation_Date.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-cd /d "C:\Photos_Demo\"
-
-for %%f in (*.jpg *.jpeg *.png *.gif *.bmp *.tiff *.webp) do (
-  for /f "tokens=2 delims==" %%I in ('powershell -Command "(Get-Item '%%f').CreationTime.ToString('yyyyMMdd')"') do set date=%%I
-  set "ext=%%~xf"
-  ren "%%f" "!date!_%%~nf!ext!"
-)
