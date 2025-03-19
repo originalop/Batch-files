26b5c50491bc5c63846bca3ac0ccd4d843ab5e30
originalop <rswami5579@gmail.com>
2025-03-19 17:35:22 +0530

    This new script can write data into notepad file and open it

---
+++ Write_Data_Into_Notepad.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Hey! this is Sweta, how can I help you? > myfile.txt
+::echo Another line of text >> myfile.txt
+notepad myfile.txt
+::> creates (or overwrites) the file.
+::>> appends to the file.
